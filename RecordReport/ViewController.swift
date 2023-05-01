a66436fb999ed9137d2ebe0ef0acc6d4402b3542
Howard-Zjun <2548795196@qq.com>
2023-05-01 23:42:12 +0800

    Initial Commit

---
+++ RecordReport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecordReport
+//
+//  Created by Howard-Zjun on 2023/5/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
